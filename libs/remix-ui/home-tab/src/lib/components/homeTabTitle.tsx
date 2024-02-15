c56b3686bbb4c2751b43e5cf0e95eac1accc3da8
bendn <https://slopythegreat@gmail.com>
2024-02-15 15:17:43 +0700

    heh

--- libs/remix-ui/home-tab/src/lib/components/homeTabTitle.tsx
+++ libs/remix-ui/home-tab/src/lib/components/homeTabTitle.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import React, {useEffect, useState, useRef, useContext} from 'react'
-import {FormattedMessage, useIntl} from 'react-intl'
-import {CustomTooltip} from '@remix-ui/helper'
+import React, { useEffect, useState, useRef, useContext } from 'react'
+import { FormattedMessage, useIntl } from 'react-intl'
+import { CustomTooltip } from '@remix-ui/helper'
 const _paq = (window._paq = window._paq || []) // eslint-disable-line
 
 function HomeTabTitle() {
@@ -53,96 +53,18 @@ function HomeTabTitle() {
     <div className="px-2 pb-2 pt-2 d-flex flex-column border-bottom" id="hTTitleSection">
       <div className="d-flex py-2 justify-content-between">
         <div className="d-flex justify-content-start">
-          <span className="h-80 text-uppercase" style={{fontSize: 'xx-large', fontFamily: 'Noah, sans-serif'}}>
+          <span className="h-80 text-uppercase" style={{ fontSize: 'xx-large', fontFamily: 'Noah, sans-serif' }}>
             Remix
           </span>
           <div className="ml-2 d-flex">
             <div onClick={() => playRemi()}>
-              <img className="" src="assets/img/guitarRemiCroped.webp" style={{height: '3rem'}} alt=""></img>
+              <img className="" src="assets/img/guitarRemiCroped.webp" style={{ height: '3rem' }} alt=""></img>
             </div>
             <audio id="remiAudio" muted={false} src="assets/audio/remiGuitar-single-power-chord-A-minor.mp3" ref={remiAudioEl}></audio>
           </div>
         </div>
-        <span className="d-flex flex-nowrap align-self-end">
-          <CustomTooltip
-            placement={'top'}
-            tooltipId="overlay-tooltip"
-            tooltipClasses="text-nowrap"
-            tooltipText={<FormattedMessage id="home.remixYoutubePlaylist" />}
-            tooltipTextClasses="border bg-light text-dark p-1 pr-3"
-          >
-            <button
-              onClick={() => {
-                openLink('https://www.youtube.com/channel/UCjTUPyFEr2xDGN6Cg8nKDaA')
-                _paq.push(['trackEvent', 'hometab', 'socialMedia', 'youtube'])
-              }}
-              className="border-0 px-1 h-100 btn fab fa-youtube"
-            ></button>
-          </CustomTooltip>
-          <CustomTooltip
-            placement={'top'}
-            tooltipId="overlay-tooltip"
-            tooltipClasses="text-nowrap"
-            tooltipText={<FormattedMessage id="home.remixTwitterProfile" />}
-            tooltipTextClasses="border bg-light text-dark p-1 pr-3"
-          >
-            <button
-              onClick={() => {
-                openLink('https://twitter.com/EthereumRemix')
-                _paq.push(['trackEvent', 'hometab', 'socialMedia', 'twitter'])
-              }}
-              className="border-0 px-1 h-100 btn fab fa-x-twitter"
-            ></button>
-          </CustomTooltip>
-          <CustomTooltip
-            placement={'top'}
-            tooltipId="overlay-tooltip"
-            tooltipClasses="text-nowrap"
-            tooltipText={<FormattedMessage id="home.remixLinkedinProfile" />}
-            tooltipTextClasses="border bg-light text-dark p-1 pr-3"
-          >
-            <button
-              onClick={() => {
-                openLink('https://www.linkedin.com/company/ethereum-remix/')
-                _paq.push(['trackEvent', 'hometab', 'socialmedia', 'linkedin'])
-              }}
-              className="border-0 px-1 h-100 btn fab fa-linkedin"
-            ></button>
-          </CustomTooltip>
-          <CustomTooltip
-            placement={'top'}
-            tooltipId="overlay-tooltip"
-            tooltipClasses="text-nowrap"
-            tooltipText={<FormattedMessage id="home.remixMediumPosts" />}
-            tooltipTextClasses="border bg-light text-dark p-1 pr-3"
-          >
-            <button
-              onClick={() => {
-                openLink('https://medium.com/remix-ide')
-                _paq.push(['trackEvent', 'hometab', 'socialmedia', 'medium'])
-              }}
-              className="border-0 h-100 px-1 btn fab fa-medium"
-            ></button>
-          </CustomTooltip>
-
-          <CustomTooltip
-            placement={'top'}
-            tooltipId="overlay-tooltip"
-            tooltipClasses="text-nowrap"
-            tooltipText={<FormattedMessage id="home.joinUsOnDiscord" />}
-            tooltipTextClasses="border bg-light text-dark p-1 pr-3"
-          >
-            <button
-              onClick={() => {
-                openLink('https://discord.gg/mh9hFCKkEq')
-                _paq.push(['trackEvent', 'hometab', 'socialmedia', 'discord'])
-              }}
-              className="border-0 h-100 pl-1 pr-0 btn fab fa-discord"
-            ></button>
-          </CustomTooltip>
-        </span>
       </div>
-      <b className="py-1 text-dark" style={{fontStyle: 'italic'}}>
+      <b className="py-1 text-dark" style={{ fontStyle: 'italic' }}>
         <FormattedMessage id="home.nativeIDE" />
       </b>
       <div className="pb-1" id="hTGeneralLinks">
@@ -165,33 +87,6 @@ function HomeTabTitle() {
         >
           <FormattedMessage id="home.remixPlugin" />
         </a>
-        <a
-          className="pl-2 remixui_home_text"
-          onClick={() => _paq.push(['trackEvent', 'hometab', 'header', 'remixDesktop'])}
-          target="__blank"
-          href="https://github.com/ethereum/remix-desktop/releases"
-        >
-          <FormattedMessage id="home.remixDesktop" />
-        </a>
-      </div>
-      <div className="d-flex pb-1 align-items-center">
-        <input
-          ref={searchInputRef}
-          type="text"
-          className="border form-control border-right-0"
-          id="homeTabSearchInput"
-          placeholder={intl.formatMessage({id: 'home.searchDocumentation'})}
-          data-id="terminalInputSearch"
-        />
-        <button
-          className="form-control border d-flex align-items-center p-2 justify-content-center fas fa-search bg-light"
-          onClick={(e) => {
-            _paq.push(['trackEvent', 'hometab', 'header', 'searchDocumentation'])
-            openLink()
-          }}
-          disabled={state.searchDisable}
-          style={{width: '3rem'}}
-        ></button>
       </div>
     </div>
   )
--- libs/remix-ui/home-tab/src/lib/remix-ui-home-tab.tsx
+++ libs/remix-ui/home-tab/src/lib/remix-ui-home-tab.tsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect, useContext} from 'react' // eslint-disable-line
+import React, { useState, useEffect, useContext } from 'react' // eslint-disable-line
 
 import './remix-ui-home-tab.css'
-import {ThemeContext, themes} from './themeContext'
+import { ThemeContext, themes } from './themeContext'
 import HomeTabTitle from './components/homeTabTitle'
 import HomeTabFile from './components/homeTabFile'
 import HomeTabLearn from './components/homeTabLearn'
@@ -14,10 +14,10 @@ import { HomeTabFileElectron } from './components/homeTabFileElectron'
 import { LanguageOptions } from './components/homeTablangOptions'
 
 
-
 declare global {
   interface Window {
     _paq: any
+    ethereum: any
   }
 }
 
@@ -25,12 +25,49 @@ export interface RemixUiHomeTabProps {
   plugin: any
 }
 
+/**
+ * add the selendra network to metamask / w/e
+ */
+async function add_sel(test: boolean = false) {
+  try {
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          chainId: "0x" + (test ? 1953 : 1961).toString(16),
+          chainName: "Selendra" + (test ? " Testnet" : ''),
+          rpcUrls: [test ? "https://rpc0-testnet.selendra.org" : "https://rpc0.selendra.org"],
+          nativeCurrency: {
+            decimals: 18,
+            name: "Selendra",
+            symbol: "SEL",
+          },
+          iconUrls: ["https://avatars.githubusercontent.com/u/49308834"]
+        }
+      ]
+    })
+  }
+  catch (e) {
+    if (e.code == 4001) {
+      alert("pls press yes?");
+    } else {
+      console.error(e)
+      alert("you haven't installed fox app!")
+    }
+  }
+}
+
+async function add_testnet() {
+  await add_sel(true)
+}
+
+
 export const RemixUiHomeTab = (props: RemixUiHomeTabProps) => {
   const platform = useContext(platformContext)
-  const {plugin} = props
+  const { plugin } = props
 
   const [state, setState] = useState<{
-    themeQuality: {filter: string; name: string}
+    themeQuality: { filter: string; name: string }
   }>({
     themeQuality: themes.light
   })
@@ -60,23 +97,38 @@ export const RemixUiHomeTab = (props: RemixUiHomeTabProps) => {
     <div className="d-flex flex-column w-100" data-id="remixUIHTAll">
       <ThemeContext.Provider value={state.themeQuality}>
         <div className="d-flex flex-row w-100 custom_home_bg">
-          <div className="px-2 pl-3 justify-content-start d-flex border-right flex-column" id="remixUIHTLeft" style={{width: 'inherit'}}>
+          <div className="px-2 pl-3 justify-content-start d-flex border-right flex-column" id="remixUIHTLeft" style={{ width: 'inherit' }}>
             <HomeTabTitle />
-            {!(platform === appPlatformTypes.desktop) ?
-              <HomeTabFile plugin={plugin} />:
-              <HomeTabFileElectron plugin={plugin}></HomeTabFileElectron>}
-            <HomeTabLearn plugin={plugin} />
           </div>
-          <div className="pl-2 pr-3 justify-content-start d-flex flex-column" style={{width: '65%'}} id="remixUIHTRight">
-            <LanguageOptions plugin={plugin}/>
-            <HomeTabFeatured></HomeTabFeatured>
-            <HomeTabGetStarted plugin={plugin}></HomeTabGetStarted>
-            <HomeTabFeaturedPlugins plugin={plugin}></HomeTabFeaturedPlugins>
-            <HomeTabScamAlert></HomeTabScamAlert>
+          <div className="pl-2 pr-3 justify-content-start d-flex flex-column" style={{ width: '65%' }} id="remixUIHTRight">
+            <LanguageOptions plugin={plugin} />
           </div>
         </div>
-      </ThemeContext.Provider>
-    </div>
+
+        <h3 style={{ marginLeft: 25 }}>
+          Getting started
+        </h3>
+        <div className="px-2 pl-3 justify-content-start d-flex border-right flex-column" id="remixUIHTLeft" style={{ width: 'inherit', marginLeft: 25 }}>
+          <ul>
+            <li>install the <a href="https://metamask.io/download/" target="_blank">Metamask</a> browser addon.</li>
+            <li>
+              add the selendra mainnet: <img onClick={async () => { await add_sel(); }} src="assets/img/selendra.svg" title="press that button" height={40}></img>
+            </li>
+            <li>
+              add the selendra testnet: <img onClick={async () => { await add_sel(true); }} src="assets/img/seltest.svg" title="press that button" height={40}></img>
+            </li>
+            <li>(please note these buttons do nothing if selendra is already added as a network)</li>
+            <li>create a new workspace</li>
+            <li>open the token solidity file</li>
+            <li>go to the <img src="assets/img/solidity.webp" className="invert dark remixui_image" height={18}></img> compile tab (its on the left)</li>
+            <li>press the "compile" button</li>
+            <li>go to the <img src="assets/img/deployAndRun.webp" className="invert dark remixui_image" height={18}></img> deploy tab</li>
+            <li>press the deploy button</li>
+            <li>profit</li>
+          </ul>
+        </div>
+      </ThemeContext.Provider >
+    </div >
   )
 }
 
